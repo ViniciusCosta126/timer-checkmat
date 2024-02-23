91a936abe5c9e26946964cc012427f3c6fa33a4f
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-02-23 18:10:52 -0300

    Configuração inicial do projeto de timer

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Timer from './src/components/timer/index.jsx';
+import Header from './src/components/header/index.jsx';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header/>
+      <View style={styles.containerTimer}>
+        <Image style={styles.img1} source={require('./assets/logo-checkmat-araraquara.png')}/>
+        <Timer />
+        <Image style={styles.img2} source={require('./assets/logo-checkmat.png')}/>
+      </View>
+      
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +19,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  containerTimer:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  img1:{
+    width: 200,
+    height: 200,
+    objectFit:'contain',
+    position:'absolute',
+    zIndex:10,
+    left: 0,
+  },
+  img2:{
+    width: 150,
+    height: 150,
+    objectFit:'contain',
+    position:'absolute',
+    right: 10,
+  }
 });
